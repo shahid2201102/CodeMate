6ded9a4a10463d9e2b6c69d2ca31ceebccca07dc
Shahid <mohdshahid643285@gmail.com>
2025-09-03 16:43:32 +0530

    implement user registration with validation and JWT generation

---
+++ backend/services/user.service.js
@@ -0,0 +1,19 @@
+import userModel from "../models/user.model.js";   
+
+
+export const createUser = async ({
+    email,
+    password
+}) => {
+
+    if(!email || !password) {
+        throw new Error("Email and password are required");
+    }
+
+    const hashedPassword = await userModel.hashPassword(password);
+    const user = new userModel({
+        email,
+        password: hashedPassword
+    });
+    return user;
+};
