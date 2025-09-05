05d27989ea9441d05110e9a16a9665642c5514e1
Shahid <mohdshahid643285@gmail.com>
2025-09-05 16:45:22 +0530

    add project model, service, and routes; implement project creation functionality

--- backend/app.js
+++ backend/app.js
@@ -2,6 +2,7 @@ import express from 'express';
 import morgan from 'morgan';
 import connect from './db/db.js'; 
 import userRoutes from './routes/user.routes.js';
+import projectRoutes from './routes/project.routes.js';
 import cookieParser from 'cookie-parser'; 
 import cors from 'cors';
 
@@ -15,6 +16,7 @@ app.use(morgan('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use('/users', userRoutes);
+app.use('/projects', projectRoutes);
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
